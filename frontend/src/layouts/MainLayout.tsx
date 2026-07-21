30eb95ce1ff09fe26774f6ff33f3de7f04677e3c
KRUZKA_COFFEE <59487003+GWANUR@users.noreply.github.com>
2026-07-21 17:43:32 +0300

    Login

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -9,48 +9,48 @@ import {
 
 export default function MainLayout() {
     return (
-        <>
-        <div className="sidebar">
-            <h2>Dashboard <br/><a href="https://github.com/GWANUR" target="_blank" rel="noopener noreferrer">By StackAlex</a></h2>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/">
-                            <LayoutDashboard />
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/servers">
-                            <Server size={18} />
-                            Servers
+        <div className="main-layout">
+            <div className="sidebar">
+                <h2>Dashboard <br/><a href="https://github.com/GWANUR" target="_blank" rel="noopener noreferrer">By StackAlex</a></h2>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">
+                                <LayoutDashboard />
+                                Dashboard
                             </a>
-                    </li>
-                    <li>
-                        <a href="/log">
-                            <Logs size={18} />
-                            Log
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/terminal">
-                            <Terminal size={18} />
-                            Terminal
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/users">
-                            <Users size={18} />
-                            Users
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+                        </li>
+                        <li>
+                            <a href="/servers">
+                                <Server size={18} />
+                                Servers
+                                </a>
+                        </li>
+                        <li>
+                            <a href="/log">
+                                <Logs size={18} />
+                                Log
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/terminal">
+                                <Terminal size={18} />
+                                Terminal
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/users">
+                                <Users size={18} />
+                                Users
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+                
+                <main>
+                    <Outlet />
+                </main>
         </div>
-            
-            <main>
-                <Outlet />
-            </main>
-        </>
     );
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,23 @@
 export default function Login() {
     return (
         <>
-            <p>This is the login page.</p>
+        <section className="pages" id="Login">
+            <h1 className="">DashBoard by StackAlex</h1>
+            <div className="login_window">
+                <h2>Log In</h2>
+                <form action="/">
+                    <div className="input_icon">
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div className="input_icon">
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <button type="submit" className="btn_icon">
+                        Log In
+                    </button>
+                </form>
+            </div>
+        </section>
         </>
     );
 }
