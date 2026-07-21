983902fc9cc687ceb9dc087aaf46121c6b5e7cc7
KRUZKA_COFFEE <59487003+GWANUR@users.noreply.github.com>
2026-07-21 17:57:27 +0300

    Login2.2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 
 Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
